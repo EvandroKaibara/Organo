d3f9d9a9086112828a58d2a054bf504f60be6510
Evandro Kaibara <yujievandro@gmail.com>
2024-02-21 21:05:26 -0300

    Projeto ate a aula 3

---
+++ src/componentes/ListaSuspensa/ListaSuspensa.js
@@ -0,0 +1,16 @@
+import './ListaSuspensa.css'
+
+function ListaSuspensa(props){
+    return(
+        <div className='lista-suspensa'>
+            <label>{props.label}</label>
+            <select onChange={evento => props.aoAlterado(evento.target.value)} required={props.obrigatorio} vakue={props.value}>
+                {props.itens.map(item => {
+                    return <option key={item}>{item}</option>
+                })}
+            </select>
+        </div>
+    );
+}
+
+export default ListaSuspensa
