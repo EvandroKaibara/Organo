66f8eae45696eb1fc80031ff78d7f909a1f11b51
Evandro Kaibara <yujievandro@gmail.com>
2024-02-22 21:06:15 -0300

    Resolver - App.js (Linha 62)

--- src/componentes/Formulario/Formulario.js
+++ src/componentes/Formulario/Formulario.js
@@ -6,15 +6,6 @@ import './Formulario.css'
 
 
 function Formulario(props) {
-    const times = [
-        'Programação',
-        'Front-End',
-        'Data Science',
-        'Devops',
-        'UX e Design',
-        'Mobile',
-        'Inovação e Gestão'
-    ]
 
     const [nome, setNome] = useState('')
     const [cargo, setCargo] = useState('')
@@ -58,7 +49,7 @@ function Formulario(props) {
                 <ListaSuspensa 
                     obrigatorio={true} 
                     label="Time" 
-                    itens={times}
+                    itens={props.times}
                     valor={time}
                     aoAlterado={valor => setTime(valor)}
                 />
